9953eaf6f39285c51723538858383bf9690a870a
LucasMonFC <lucas.monteiro.oficial@gmail.com>
2026-02-22 10:17:02 -0300

    Versão 1.1.0

--- MWC_Localization_Core.cs
+++ MWC_Localization_Core.cs
@@ -12,7 +12,7 @@ namespace MWC_Localization_Core
         public override string ID => "MWC_Localization_Core_BR";
         public override string Name => "MWC_Localization_Core";
         public override string Author => "potatosalad775&LucasMonOficial";
-        public override string Version => "1.0.9";
+        public override string Version => "1.1.0";
         public override string Description => "Multi-language core localization framework for My Winter Car";
         public override Game SupportedGames => Game.MyWinterCar;
 
