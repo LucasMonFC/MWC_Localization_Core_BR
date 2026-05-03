51045bb4bf3c9008d9567f4893f95907569dd4bb
LucasMonFC <121443731+LucasMonFC@users.noreply.github.com>
2026-05-02 21:16:56 -0300

    r1.3.3

--- MWC_Localization_Core.cs
+++ MWC_Localization_Core.cs
@@ -8,10 +8,10 @@ namespace MWC_Localization_Core
     public class MWC_Localization_Core : Mod
     {
         // Mod metadata
-        public override string ID => "MWC_Localization_Core";
+        public override string ID => "MWC_Localization_Core_BR";
         public override string Name => "MWC_Localization_Core";
-        public override string Author => "potatosalad775";
-        public override string Version => "1.0.9";
+        public override string Author => "potatosalad775&LucasMonOficial";
+        public override string Version => "1.3.3";
         public override string Description => "Multi-language core localization framework for My Winter Car";
         public override Game SupportedGames => Game.MyWinterCar;
 
